993f46733fb28b2a2d6a6e8bc6e0dc383bea19d3
aselwilliams <aselwilliams22@gmail.com>
2022-08-23 03:04:44 -0500

    handling props in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
+import List from './components/List'
 
 interface IState {
   people: {
@@ -13,12 +14,11 @@ interface IState {
 function App() {
 const [people, setPeople] = useState<IState['people']>([])
 
-people.map(person => {
-person.note
-})
+
   return (
     <div className="App">
       <h1>People invited to my Party</h1>
+      <List people={people}/>
     </div>
   );
 }
