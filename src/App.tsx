43bd5a0d783e3d109f9773c707aec819d07b441e
aselwilliams <aselwilliams22@gmail.com>
2022-08-23 04:37:56 -0500

    handling events in ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import AddToList from './components/AddToList';
 import List from './components/List'
 
-interface IState {
+ export interface IState {
   people: {
     name: string
     age: number
@@ -27,7 +27,7 @@ const [people, setPeople] = useState<IState['people']>([
     <div className="App">
       <h1>People invited to my Party</h1>
       <List people={people}/>
-      <AddToList />
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {IState as Props} from '../App';
 
-const AddToList = () => {
+interface IProps {
+    people: Props['people']
+    setPeople: React.Dispatch<React.SetStateAction<Props['people']>>
+  }
+
+const AddToList: React.FC<IProps> = ({people, setPeople}) => {
     const [input, setInput] = useState({
         name: '',
         age: '',
@@ -8,18 +14,39 @@ const AddToList = () => {
         img: ''
     })
 
-    const handleChange= (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const handleChange= (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
 setInput({
     ...input,
     [e.target.name]: e.target.value
 })
     }
+
+    const handleClick = (): void => {
+        if(!input.name || !input.age || !input.img){
+            return 
+        }
+        setPeople([
+            ...people, {
+                name: input.name,
+                age: parseInt(input.age),
+                url: input.img,
+                note: input.note
+            }
+        ])
+        setInput({
+            name: '',
+            age: '',
+            note: '',
+            img: ''
+        })
+    }
     return (
         <div className='AddToList'>
             <input type='text' placeholder='Name' className='AddToList-input' value={input.name} onChange={handleChange} name='name'/>
-            <input type='text' placeholder='Age' className='AddToList-input' value={input.age} onChange={handleChange} name='age'/>
+            <input type='number' placeholder='Age' className='AddToList-input' value={input.age} onChange={handleChange} name='age'/>
             <input type='text' placeholder='Image Url' className='AddToList-input' value={input.img} onChange={handleChange} name='img'/>
             <textarea placeholder='Notes' className='AddToList-input' value={input.note} onChange={handleChange} name='note'/>
+            <button className='AddToList-btn' onClick={handleClick}>Add to List</button>
         </div>
     )
 }
